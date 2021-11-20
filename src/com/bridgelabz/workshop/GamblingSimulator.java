1153b79f05caa67595c76ef49b8e6bfda839b902
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-11-20 19:18:22 +0530

    Refractor adding condition to play next month if player wins the current month only

--- src/com/bridgelabz/workshop/GamblingSimulator.java
+++ src/com/bridgelabz/workshop/GamblingSimulator.java
@@ -1,7 +1,5 @@
 package com.bridgelabz.workshop;
 
-import java.util.Scanner;
-
 public class GamblingSimulator {
 
 	public static void main(String[] args) {
@@ -9,10 +7,6 @@ public class GamblingSimulator {
 		System.out.println("---welcome to Gambling Simulator Program---\n");
 		int stake = 100;
 		int percentage = 50; // percentage limit of gain or loss
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter no of months to play Gambling: ");
-		int months = sc.nextInt();
-
 		System.out.println(
 				" your initial stake amount to start betting every day: " + stake + "$" + "\n Every game bet: 1$\n");
 		int max_Stake = stake + (int) ((percentage / (double) 100) * stake); // amount limit after 50% gain
@@ -20,15 +14,16 @@ public class GamblingSimulator {
 
 		GamblingServices gambler = new GamblingServices();
 
-		for (int month = 1; month <= months; month++) {
-			boolean win_status=gambler.monthlyBet(stake, max_Stake, min_Stake, month);
-			if(!win_status) {
+		int month = 1;
+		boolean win_status = true;
+		while (win_status) {
+			win_status = gambler.monthlyBet(stake, max_Stake, min_Stake, month);
+			month++;
+			if (win_status == false) {
 				break;
-			}
-			else {
+			} else {
 				continue;
 			}
 		}
-		sc.close();
 	}
 }
